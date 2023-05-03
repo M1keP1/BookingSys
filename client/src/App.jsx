df78acdc6974b98cde18f3dd54a8ecb00f48a28f
M1keP1 <mihirkumar654@gmail.com>
2023-05-03 17:41:50 +0200

    Wireframed Top-header
    added color profile : secondary-Red

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,44 @@ import './App.css'
 function App() {
   return (
     
-    <div className='bg-red-500'><h1>This is a test</h1> </div>
-      
+    <div>
+      <header className='p-5 flex justify-between'>
+        <a href='' className='flex items-center gap-1'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ff0800" className="w-8 h-8">
+          <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+        </svg>
+
+        <span className='font-bold text-xl'>Denty</span>
+
+
+        </a>
+        <div className='flex gap-1.5 border border-gray-300 rounded-full p-3.5 shadow-md shadow-red-300 '>
+          <div className='relative top-2'>Anywhere</div>
+          <div className="border-l border-secondary"></div>
+          <div className='relative top-2'>Anytime</div>
+          <div className="border-l border-secondary"></div>
+          <div className='relative top-2'>Single Booking</div>
+          <button className='bg-secondary p-2 rounded-full'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+
+          </button>
+        </div>
+        <div className="flex gap-2 border border-gray-300 rounded-full p-3 shadow-md shadow-red-300">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 relative top-2">
+          <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+        </svg>
+        <div className="bg-secondary text-white rounded-full p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+        </svg>
+
+        </div>
+
+        </div>
+      </header>
+    </div>
       
     
   )
